2f8a91f108a4f86bbebffc797b1925abf607cc5a
Rilton <bobvanelst@gmail.com>
2025-04-16 18:46:37 +0200

    Updated serializer test with svut

--- src/serializer.sv
+++ src/serializer.sv
@@ -1,47 +1,75 @@
-`default_nettype none 
+`default_nettype none
 /* verilator lint_off WIDTHEXPAND */
 // No native support for 3.3v hdmi differential so we fake it
 
 module serializer #(
-    parameter c_data_width = 10,
-    parameter bit c_ddr_mode = 1 
+    parameter int p_data_width = 10,
+    parameter bit p_ddr_mode = 1
 )(
+    input  logic i_clk_data,
     input  logic i_clk_shift,
     input  logic i_rst,
-    input  logic [c_data_width-1:0] i_data,
+    input  logic [p_data_width-1:0] i_data,
     output logic [1:0] o_data, //for Single Data Rate, you just use [0]
     output logic [1:0] o_clk //for Single Data Rate, you just use [0]
 );
-    logic [c_data_width-1:0] s_clk_out, s_data_shift;
+    logic [p_data_width-1:0] s_clk_out, s_data_shift;
+    logic s_shift_clock_in_sync;
+    logic s_skip_clk_shift;
+    logic s_data_clock_old;
 
-    localparam c_init_count = {{(c_data_width/2){1'b0}}, {(c_data_width/2){1'b1}}};
-
+    localparam logic [p_data_width-1:0] c_init_count =
+        {{(p_data_width/2){1'b0}}, {(p_data_width/2){1'b1}}} << 1+ p_ddr_mode;
 
     assign o_data = s_data_shift [1:0];
     assign o_clk = s_clk_out [1:0];
 
+    // assign s_shift_clock_in_sync = (s_clk_out == c_init_count);
+    always_ff @(posedge i_clk_data) begin
+        if (i_rst) begin
+            s_shift_clock_in_sync <= '0;
+        end else begin
+            //At the rising ede of i_clk_data, the shift clock should always be
+            //equal to the init value, otherwise it's out of sync.
+            s_shift_clock_in_sync <= (s_clk_out == c_init_count);
+        end
+    end
+
     always_ff @(posedge i_clk_shift) begin
         if (i_rst) begin
+            s_data_clock_old <= '0;
             s_clk_out <= c_init_count;
             s_data_shift <= '0;
+            s_skip_clk_shift <= '0;
         end else begin
-            if (c_ddr_mode) begin
-                //shift to the right by two, ring buffer style
-                s_clk_out <= {s_clk_out[1:0], s_clk_out[c_data_width-1:2]};
+            s_data_clock_old <= i_clk_data; //save old clock data
+
+            //falling edge detection
+            s_skip_clk_shift <= i_clk_data == 1'b0
+                        & s_data_clock_old == 1'b1
+                        & ~(s_shift_clock_in_sync);
+
+            if (~s_skip_clk_shift) begin
+                if (p_ddr_mode) begin
+                    //shift to the right by two, ring buffer style
+                    s_clk_out <= {s_clk_out[1:0], s_clk_out[p_data_width-1:2]};
+                end else begin
+                    //shift to the right by one, ring buffer style
+                    s_clk_out <= {s_clk_out[0], s_clk_out[p_data_width-1:1]};
+                end
             end else begin
-                //shift to the right by one, ring buffer style
-                s_clk_out <= {s_clk_out[0], s_clk_out[c_data_width-1:1]};
+                s_clk_out <= s_clk_out;
             end
 
             if (s_clk_out == c_init_count) begin
                 s_data_shift <= i_data;
             end else begin
-                if (c_ddr_mode) begin
+                if (p_ddr_mode) begin
                     //shift to the right by two, ring buffer style
-                    s_data_shift <= {s_data_shift[1:0], s_data_shift[c_data_width-1:2]};
+                    s_data_shift <= {s_data_shift[1:0], s_data_shift[p_data_width-1:2]};
                 end else begin
                     //shift to the right by one, ring buffer style
-                    s_data_shift <= {s_data_shift[0], s_data_shift[c_data_width-1:1]};
+                    s_data_shift <= {s_data_shift[0], s_data_shift[p_data_width-1:1]};
                 end
             end
         end
--- src/test/tb_serializer.sv
+++ src/test/tb_serializer.sv
@@ -1,48 +1,220 @@
-`default_nettype none 
-// No native support for 3.3v hdmi differential so we fake it
+// Mandatory file to be able to launch SVUT flow
+`include "svut_h.sv"
+// Specify the module to load or on files.f
+`include "serializer.sv"
+`timescale 1 ns / 100 ps
 
-module tb_serializer;
+module tb_serializer();
 
-    logic clk = 0, rst = 1;
-    logic [9:0] i_data;
-    logic [1:0] o_data;
-    logic [1:0] o_clk;
+    `SVUT_SETUP
 
-    always #5 clk = ~clk; // verilator lint_off STMTDLY
+    parameter p_data_width = 10;
 
-    serializer dut (
-        .i_clk_shift(clk),
-        .i_rst(rst),
-        .i_data(i_data),
-        .o_data(o_data),
-        .o_clk(o_clk)
+    logic clk_data = 1, clk_sdr = 1, clk_ddr = 1, rst = 1;
+    logic [p_data_width-1:0] i_data;
+    logic [1:0] o_data_ddr, o_data_sdr;
+    logic [1:0] o_clk_ddr, o_clk_sdr;
+
+    serializer #(
+        .p_data_width (p_data_width),
+        .p_ddr_mode   (1)
+    ) dut_ddr (
+        .i_clk_data  (clk_data),
+        .i_clk_shift (clk_ddr),
+        .i_rst       (rst),
+        .i_data      (i_data),
+        .o_data      (o_data_ddr),
+        .o_clk       (o_clk_ddr)
     );
 
+    serializer #(
+        .p_data_width (p_data_width),
+        .p_ddr_mode   (0)
+    ) dut_sdr (
+        .i_clk_data  (clk_data),
+        .i_clk_shift (clk_sdr),
+        .i_rst       (rst),
+        .i_data      (i_data),
+        .o_data      (o_data_sdr),
+        .o_clk       (o_clk_sdr)
+    );
+    task automatic wait_cycles_data(input int n);
+        repeat (n) @(posedge clk_data);
+    endtask
+    task automatic wait_cycles_ddr(input int n);
+        repeat (n) @(posedge clk_ddr);
+    endtask
+    task automatic wait_cycles_sdr(input int n);
+        repeat (n) @(posedge clk_sdr);
+    endtask
+
+    //Clocks
+    always #(5ns) clk_sdr = ~clk_sdr; // verilator lint_off STMTDLY
+    always #(10ns) clk_ddr = ~clk_ddr; // verilator lint_off STMTDLY
+    always #(50ns) clk_data = ~clk_data; // verilator lint_off STMTDLY
+
+    // To dump data for visualization:
     initial begin
-        //setup waveform dump
-        $dumpfile("wave.vcd");
-        $dumpvars(0,tb_serializer);
-        $dumpvars(0,dut);
-
-        //init
-        rst = 1;
-        i_data = '0;
-        wait_cycles(10);
-        rst = 0;
-        i_data = 'b1;
-        // i_data = 'b1111000011;
-        wait_cycles(3);
-        // i_data = '1;
-        wait_cycles(2);
-        // i_data = 'b1000100001;
-        wait_cycles(10);
-        wait_cycles(10);
-        i_data = '0;
-
-        $finish;
+        $dumpfile("wave.fst");
+        $dumpvars(0, tb_serializer);
     end
 
-    task automatic wait_cycles(input int n);
-        repeat (n) @(posedge clk);
+    // Setup time format when printing with $realtime()
+    initial $timeformat(-9, 1, "ns", 8);
+
+    task setup(msg="");
+    begin
+        rst = 1
+        i_data = '0;
+        wait_cycles_data(4);
+        rst = 0;
+    end
     endtask
+
+    // teardown() runs when a test ends
+    task teardown(msg="");
+    begin
+        wait_cycles_data(4);
+        rst = 1;
+        i_data = '0;
+        wait_cycles_data(4);
+    end
+    endtask
+
+    `TEST_SUITE("TESTSUITE_NAME")
+
+    //  Available macros:"
+    //
+    //    - `MSG("message"):       Print a raw white message
+    //    - `INFO("message"):      Print a blue message with INFO: prefix
+    //    - `SUCCESS("message"):   Print a green message if SUCCESS: prefix
+    //    - `WARNING("message"):   Print an orange message with WARNING: prefix and increment warning counter
+    //    - `CRITICAL("message"):  Print a purple message with CRITICAL: prefix and increment critical counter
+    //    - `FAILURE("message"):   Print a red message with FAILURE: prefix and do **not** increment error counter
+    //    - `ERROR("message"):     Print a red message with ERROR: prefix and increment error counter
+    //
+    //    - `FAIL_IF(aSignal):                 Increment error counter if evaluaton is true
+    //    - `FAIL_IF_NOT(aSignal):             Increment error coutner if evaluation is false
+    //    - `FAIL_IF_EQUAL(aSignal, 23):       Increment error counter if evaluation is equal
+    //    - `FAIL_IF_NOT_EQUAL(aSignal, 45):   Increment error counter if evaluation is not equal
+    //    - `ASSERT(aSignal):                  Increment error counter if evaluation is not true
+    //    - `ASSERT(aSignal == 0):           Increment error counter if evaluation is not true
+    //
+    //  Available flag:
+    //
+    //    - `LAST_STATUS: tied to 1 if last macro did experience a failure, else tied to 0
+    //
+    `UNIT_TEST("sdr_data shift_check multiple")
+        #1;
+        begin
+            fork
+            begin
+                i_data = 'b1101010101;
+                repeat(5) begin
+                    wait_cycles_data(1);
+                    i_data = ~i_data;
+                end
+            end
+            begin
+                wait(dut_sdr.s_shift_clock_in_sync);
+                wait_cycles_data(1);
+                repeat(5) begin
+                    for(int j = 0; j < 10; j++) begin
+                        #1;
+                        // $display("%b != %b",o_data_sdr[0], i_data[j]);
+                        `FAIL_IF_NOT_EQUAL(o_data_sdr[0], i_data[j])
+                        wait_cycles_sdr(1);
+                    end
+                end
+            end
+            join
+        end
+    `UNIT_TEST_END
+    `UNIT_TEST("ddr_data shift_check multiple")
+        #1;
+        begin
+            fork
+            begin
+                i_data = 'b1101010101;
+                repeat(5) begin
+                    wait_cycles_data(1);
+                    i_data = ~i_data;
+                end
+            end
+            begin
+                wait(dut_ddr.s_shift_clock_in_sync);
+                wait_cycles_data(1);
+                repeat(5) begin
+                    for(int i = 0; i < 5; i++) begin
+                        #1;
+                        // $info("%d",i);
+                        // $display("%b != %b",o_data_ddr, i_data[i*2+:2]);
+                        `FAIL_IF_NOT_EQUAL(o_data_ddr, i_data[i*2+:2])
+                        wait_cycles_ddr(1);
+                    end
+                end
+            end
+            join
+        end
+    `UNIT_TEST_END
+    `UNIT_TEST("sdr_data shift_check")
+        #1;
+        begin
+            wait_cycles_data(1);
+            i_data = 'b000001;
+            wait_cycles_data(1);
+            wait(dut_sdr.s_shift_clock_in_sync);
+            wait_cycles_data(1);
+            for(int j = 0; j < 10; j++) begin
+                #1;
+                // $display("%b != %b",o_data_sdr[0], i_data[j]);
+                `FAIL_IF_NOT_EQUAL(o_data_sdr[0], i_data[j])
+                wait_cycles_sdr(1);
+            end
+        end
+    `UNIT_TEST_END
+    `UNIT_TEST("ddr_data shift_check")
+        #1;
+        begin
+            wait_cycles_data(1);
+            i_data = 'b0000011111;
+            wait_cycles_data(1);
+            wait(dut_ddr.s_shift_clock_in_sync);
+            wait_cycles_data(1);
+            for(int i = 0; i < 5; i++) begin
+                #1;
+                // $info("%d",i);
+                // $display("%b != %b",o_data_ddr, i_data[i*2+:2]);
+                `FAIL_IF_NOT_EQUAL(o_data_ddr, i_data[i*2+:2])
+                wait_cycles_ddr(1);
+            end
+        end
+    `UNIT_TEST_END
+
+    // `UNIT_TEST("ddr_data shift_check")
+    //     #10;
+    //     fork
+    //     begin
+    //         wait_cycles_data(1);
+    //         i_data = 'b0000011111;
+    //         repeat(10) begin
+    //             wait_cycles_data(1);
+    //             // i_data = ~i_data;
+    //         end
+    //         i_data = '0;
+    //     end
+    //     begin
+    //         // i_data = 'b1111000011;
+    //         wait_cycles_ddr(3);
+    //         // i_data = '1;
+    //         wait_cycles_ddr(2);
+    //         // i_data = 'b1000100001;
+    //         wait_cycles_ddr(10);
+    //         wait_cycles_ddr(100);
+    //     end
+    //     join
+    // `UNIT_TEST_END
+
+    `TEST_SUITE_END
+
 endmodule
